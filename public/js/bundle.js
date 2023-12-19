c4a155e36d58395b8458a5b575aaf9028715a858
Artem Kasperovich <artem.kas.cfc@gmail.com>
2023-12-19 09:46:51 +0300

    shopping cart page demo is created

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6114,7 +6114,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62536" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57100" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
